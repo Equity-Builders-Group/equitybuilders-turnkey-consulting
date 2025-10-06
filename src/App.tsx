f532e36b09a06b81d9a5f7b4cc7aaa3841ae4027
Lavon Woods <lavon@hybridmindset.com>
2025-10-06 03:31:06 -0500

    Making a commit

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/class-replay" element={<ClassReplay />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            { /* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */ }
             <Route path="*" element={<NotFound />} />
           </Routes>
           
